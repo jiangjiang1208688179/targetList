94b0683098ee5241fbfc8517385d37020e3c11bb
jiangjiang <1208688179@qq.com>
2018-12-13 06:37:14 -0500

    item repair

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,6 @@
         <!-- 该事件不能放在el-cart组件上，会失效 -->
         <div slot="header" class="clearfix">
           <span>{{item.listId}}</span>
-          <el-button style="float: right; padding: 3px 0" type="text">所有列表</el-button>
         </div>
         <div class="cardContain">
           <div style="margin-bottom:10px;">
@@ -21,11 +20,12 @@
           <div v-for="(item1,index1) in item.target" :key="index1" class="text item">
             <div class="targetItem" @mouseover="mouseOver(index,index1)" @mouseout="mouseOut(index, index1)">
               <div style="width:30px;height:30px" class="checkBoxStatus" @click="delectItem(index,index1)" v-bind:class="{checkBoxStatus1:item1.status}"></div>
-              <div v-bind:class="{targetItemStatus:item1.status}">{{item1.name}}</div>
-              <!-- 编辑删除操作 -->
-              <div class="editDeleteIco" v-show='item1.mouseOver'>
+              <div v-show="!item1.isEdite" v-bind:class="{targetItemStatus:item1.status}">{{item1.name}}</div>
+              <!-- 移动、编辑删除操作 -->
+              <div class="editDeleteIco" v-show='item1.mouseOver&&!item1.isEdite&&index==nowBox || (pull.a==index && pull.b==index1)'>
                 <!-- 任务移动操作 -->
-                <el-dropdown @command="changeTargetBox(index1, index)">
+                <!-- command事件本身有一个回调事件，在添加其它参数时，需要使用$event，来代表它自带的回调事件，否则会出现找不到参数的情况，若不添加其它参数，$event可省略 -->
+                <el-dropdown @command="changeTargetBox($event, index, index1)">
                   <span class="el-dropdown-link">
                     移动
                     <!-- <i class="el-icon-arrow-down el-icon--right"></i> -->
@@ -38,6 +38,11 @@
                 </el-dropdown>
                 <i class="el-icon-edit" @click="editItem(index, index1)"></i>
                 <i class="el-icon-delete" @click="deleteItem(index, index1)"></i>
+              </div>
+              <div v-show="item1.isEdite">
+                <input type="text" v-model="editeTargetName" class="createInput">
+                <el-button size="mini" @click="saveEditeItem(index, index1)">确定</el-button>
+                <el-button size="mini" @click="editecancel(index, index1)">取消</el-button>
               </div>
             </div>
           </div>
@@ -56,11 +61,13 @@ export default {
   name: "home",
   data() {
     return {
-      nowBox: 1,
+      nowBox: 0,
       createItem: [false, false, false],
       createTargetName: "",
       checked: false,
       checkBox: false,
+      editeTargetName: "",
+      pull: {a:-1,b:-1},
       // mouseOver: false,   //鼠标经过控制删除和编辑按钮
       cardList: [
         {
@@ -72,7 +79,8 @@ export default {
               isDelete: false,
               completeDate: "",
               chooseList: 0,
-              mouseOver: false
+              mouseOver: false,
+              isEdite: false
             },
             {
               name: "the second",
@@ -80,7 +88,8 @@ export default {
               isDelete: false,
               completeDate: "",
               chooseList: 0,
-              mouseOver: false
+              mouseOver: false,
+              isEdite: false
             },
             {
               name: "the thire",
@@ -88,7 +97,8 @@ export default {
               isDelete: false,
               completeDate: "",
               chooseList: 0,
-              mouseOver: false
+              mouseOver: false,
+              isEdite: false
             }
           ]
         },
@@ -100,21 +110,27 @@ export default {
               status: false,
               isDelete: false,
               completeDate: "",
-              chooseList: 0
+              chooseList: 0,
+              mouseOver: false,
+              isEdite: false
             },
             {
               name: "the second1",
               status: false,
               isDelete: false,
               completeDate: "",
-              chooseList: 0
+              chooseList: 0,
+              mouseOver: false,
+              isEdite: false
             },
             {
               name: "the thire1",
               status: false,
               isDelete: false,
               completeDate: "",
-              chooseList: 0
+              chooseList: 0,
+              mouseOver: false,
+              isEdite: false
             }
           ]
         },
@@ -126,21 +142,27 @@ export default {
               status: false,
               isDelete: false,
               completeDate: "",
-              chooseList: 0
+              chooseList: 0,
+              mouseOver: false,
+              isEdite: false
             },
             {
               name: "the second2",
               status: false,
               isDelete: false,
               completeDate: "",
-              chooseList: 0
+              chooseList: 0,
+              mouseOver: false,
+              isEdite: false
             },
             {
               name: "the thire2",
               status: false,
               isDelete: false,
               completeDate: "",
-              chooseList: 0
+              chooseList: 0,
+              mouseOver: false,
+              isEdite: false
             }
           ]
         }
@@ -180,6 +202,13 @@ export default {
       }
       this.cancel(index);
     },
+    editItem(index, index1) {
+      console.log(index, index1);
+      this.cardList[index].target[index1].isEdite = true;
+      var now = this.cardList[index].target[index1];
+      this.editeTargetName = now.name;
+      this.cardList[index].target.splice(index1, 1, now);
+    },
     cancel(index) {
       this.createItem.splice(index, 1, false);
       // this.createItem[index] = false;    //使用该方法数组元素改变，但是不会在视图上显示
@@ -190,6 +219,8 @@ export default {
     //单个target相关操作
     mouseOver(index, index1) {
       var now;
+      this.pull.a=index;
+      this.pull.b=index1;
       this.cardList[index].target[index1].mouseOver = true;
       now = this.cardList[index];
       this.cardList.splice(index, 1, now);
@@ -204,29 +235,43 @@ export default {
     deleteItem(index, index1) {
       this.cardList[index].target.splice(index1, 1);
     },
-    changeTargetBox(command, index, index1) {
-      console.log(command, index, index1)
-      if (command != index) {
+    changeTargetBox(event, index, index1) {
+      console.log(event, index, index1);
+      if (event != index) {
         var target = this.cardList[index].target[index1];
-        console.log(target)
-        if (command == "0") {
+        console.log(target);
+        if (event == "0") {
           this.cardList[0].target.unshift(target);
-          this.cardList[index].target.splice(index1,1);
+          this.cardList[index].target.splice(index1, 1);
         }
-        if (command == "1") {
+        if (event == "1") {
           this.cardList[1].target.unshift(target);
-          this.cardList[index].target.splice(index1,1);
+          this.cardList[index].target.splice(index1, 1);
         }
-        if (command == "2") {
+        if (event == "2") {
           this.cardList[2].target.unshift(target);
-          this.cardList[index].target.splice(index1,1);
+          this.cardList[index].target.splice(index1, 1);
         }
       }
+    },
+    saveEditeItem(index, index1) {
+      if (this.editeTargetName) {
+        this.cardList[index].target[index1].name = this.editeTargetName;
+        this.editecancel(index, index1)
+      }
+    },
+    editecancel(index, index1) {
+      this.cardList[index].target[index1].isEdite = false;
+        var now = this.cardList[index].target[index1];
+        this.cardList[index].target.splice(index1, 1, now);
     }
   }
 };
 </script>
 <style>
+body {
+  display: absolute;
+}
 .cardContain {
   text-align: left;
   margin-top: 0px;
